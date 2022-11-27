0451167450088e727e5d11acb34e23340a1f0f85
Gustavo do Amaral <64814431+gustavodoamaral@users.noreply.github.com>
2022-11-27 18:37:51 -0300

    Conexão com o banco de dados - mongoDB

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-//script backend javascript
+const express = require("express");
+const path = require("path");
+const routes = require("./routes/routes");
+const connectToDb = require("./database/db");
 
-const express = require('express'); // required: busca dentro do node_modules o express.
-const path = require("path"); // importação do path: responsável por redirecionar as pastas, ex: conectar o CSS ao HTML
-const routes = require("./routes/routes")
+connectToDb();
+const app = express();
+const port = 3000;
 
-const app = express(); // recebe o express sendo executado. 
-const port = 3000
-
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
-app.use(routes)
+app.use(routes);
 
-
-app.listen(3000, () => 
-console.log(`Servidor rodando em http://localhost:${port}`)
-); 
+app.listen(3000, () =>
+  console.log(`Servidor rodando em http://localhost:${port}`)
+);
