35f71a366351c810d478aff3282f82d3030ec358
Gustavo do Amaral <64814431+gustavodoamaral@users.noreply.github.com>
2023-04-11 09:54:01 -0300

    Marcando tarefa como feita com persistencia no BD

--- controller/TaskController.js
+++ controller/TaskController.js
@@ -80,10 +80,25 @@ const deleteOneTask = async (req, res) => {
   }
 }
 
+const taskCheck = async (req, res) => {
+  try{
+    const task = await Task.findOne({ _id: req.params.id });
+
+    //Condicional tenário
+    task.check ? task.check = false : task.check = true;
+
+    await Task.updateOne({ _id: req.params.id}, task)
+    res.redirect("/")
+  } catch (err) {
+    res.status(500).send({ error: err.message });
+  }
+}
+
 module.exports = {
   getAllTasks,
   createTask,
   getById,
   updateOneTask,
-  deleteOneTask
+  deleteOneTask,
+  taskCheck
 };
