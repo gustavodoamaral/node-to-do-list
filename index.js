cdc59fd9fdc3ffac9c0df49c3f3cf9c6cc4add8d
Gustavo do Amaral <64814431+gustavodoamaral@users.noreply.github.com>
2023-04-13 09:20:48 -0300

    configurações de deploy

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const express = require("express");
 const path = require("path");
 const routes = require("./routes/routes");
@@ -5,13 +7,13 @@ const connectToDb = require("./database/db");
 
 connectToDb();
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded());
 app.use(routes);
 
-app.listen(3000, () =>
+app.listen(port, () =>
   console.log(`Servidor rodando em http://localhost:${port}`)
 );
